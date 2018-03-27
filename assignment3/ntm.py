5f7bc2aa0517f8844ff6c77225c51ff67533449c
remilepriol <remi.lp.17@gmail.com>
2018-03-27 17:59:34 -0400

    Format ntm file

--- assignment3/ntm.py
+++ assignment3/ntm.py
@@ -1,32 +1,32 @@
+import numpy as np
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torch.autograd import Variable
-import torch.nn.functional as F
-import ipdb
-import numpy as np
+
 
 def circular_conv(w, s):
     circular_w = torch.cat([w[:, -1].unsqueeze(1), w, w[:, 0].unsqueeze(1)], 1)
-    ans =  s[:, 2].unsqueeze(1) * circular_w[:, :-2]
+    ans = s[:, 2].unsqueeze(1) * circular_w[:, :-2]
     ans = ans + s[:, 1].unsqueeze(1) * circular_w[:, 1:-1]
     ans = ans + s[:, 0].unsqueeze(1) * circular_w[:, 2:]
     return ans
+
 
 class NTM(nn.Module):
 
     def __init__(self, N, M, in_size, out_size, batch_size, lstm=False):
         super(NTM, self).__init__()
         self.controller = Controller(in_size, M, out_size, lstm)
-        self.read_head   = Head(batch_size, N, M)
+        self.read_head = Head(batch_size, N, M)
         self.write_head = Head(batch_size, N, M)
         self.batch_size = batch_size
         self.N = N
         self.M = M
         self.eps = 1e-8
         self.memory = None
-        self.memory_bias = Variable(torch.randn(1, N, M).cuda(),\
-                requires_grad=True)/np.sqrt(N)
-
+        self.memory_bias = Variable(torch.randn(1, N, M).cuda(), \
+                                    requires_grad=True) / np.sqrt(N)
 
     def reset(self):
         self.memory = self.memory_bias.repeat(self.batch_size, 1, 1).clone()
@@ -38,26 +38,25 @@ class NTM(nn.Module):
         return r.squeeze()
 
     def write(self, e, a):
-        tilde_memory = self.memory*(1-self.write_head.attention.unsqueeze(2)*e.unsqueeze(1))
-        self.memory = tilde_memory + self.write_head.attention.unsqueeze(2)*a.unsqueeze(1)
+        tilde_memory = self.memory * (1 - self.write_head.attention.unsqueeze(2) * e.unsqueeze(1))
+        self.memory = tilde_memory + self.write_head.attention.unsqueeze(2) * a.unsqueeze(1)
 
     def addressing(self, param, head):
         k, beta, gate, shift, gamma = param
         # Content addressing
         K_matrix = F.cosine_similarity(k.unsqueeze(1), self.memory, dim=2)
-        w_c = F.softmax(beta*K_matrix, dim=-1)
+        w_c = F.softmax(beta * K_matrix, dim=-1)
 
         # Interpolation
-        w_g = gate*w_c + (1-gate)*head.attention
+        w_g = gate * w_c + (1 - gate) * head.attention
 
         # Convolutional shift
         tilde_w = circular_conv(w_g, shift)
 
         # Sharpening
-        tilde_sum = (tilde_w**gamma).sum(1, keepdim=True)
-        w_gamma = tilde_w**gamma/(self.eps + tilde_sum)
+        tilde_sum = (tilde_w ** gamma).sum(1, keepdim=True)
+        w_gamma = tilde_w ** gamma / (self.eps + tilde_sum)
         head.attention = w_gamma
-
 
     def send(self, x):
         h = self.controller(x)
@@ -74,6 +73,7 @@ class NTM(nn.Module):
         out = self.controller.compute_output(r)
         return out
 
+
 class Head(nn.Module):
 
     def __init__(self, batch_size, N, M):
@@ -86,8 +86,9 @@ class Head(nn.Module):
         self.erase_layer = nn.Linear(100, M)
         self.add_layer = nn.Linear(100, M)
         self.attention = None
-        self.attention_score_bias = Variable(torch.randn(1,\
-            N),requires_grad=True).cuda()/np.sqrt(N)
+        self.attention_score_bias = Variable(torch.randn(1, \
+                                                         N), requires_grad=True).cuda() / np.sqrt(
+            N)
         self.batch_size = batch_size
 
     def compute_attention_params(self, h):
@@ -104,8 +105,9 @@ class Head(nn.Module):
         return e, a
 
     def reset(self):
-        self.attention = F.softmax(self.attention_score_bias,\
-                dim=1).repeat(self.batch_size, 1).clone()
+        self.attention = F.softmax(self.attention_score_bias, \
+                                   dim=1).repeat(self.batch_size, 1).clone()
+
 
 class Controller(nn.Module):
 
@@ -121,22 +123,22 @@ class Controller(nn.Module):
     def compute_output(self, r):
         return F.sigmoid(self.output_layer(r))
 
+
 dim = 8
-input = Variable(torch.bernoulli(torch.rand(32, 20, dim+1))).cuda()
+input = Variable(torch.bernoulli(torch.rand(32, 20, dim + 1))).cuda()
 input[:, :, -1] = 0
 input[:, -1, -1] = 1
 
-input_zero = Variable(torch.zeros(32, dim+1)).cuda()
+input_zero = Variable(torch.zeros(32, dim + 1)).cuda()
 
 M = 64
 N = 512
 
-ntm = NTM(N, M, dim+1, dim+1, batch_size=32)
+ntm = NTM(N, M, dim + 1, dim + 1, batch_size=32)
 ntm.cuda()
 
 criterion = torch.nn.BCELoss()
 opt = torch.optim.Adam(ntm.parameters(), lr=1e-4)
-
 
 for epoch in range(10000):
     loss = 0
@@ -146,20 +148,13 @@ for epoch in range(10000):
         ntm.send(input[:, i, :])
 
     for i in range(20):
-        #x = ntm.receive()
+        # x = ntm.receive()
         x = ntm.receive(input_zero)
         loss += criterion(x, input[:, i, :])
         acc += (x.round() == input[:, i, :]).float().mean()[0]
 
-
-
-    print('Loss:', loss.data[0]/20)
-    print('Accuracy:', acc.data[0]/20)
+    print('Loss:', loss.data[0] / 20)
+    print('Accuracy:', acc.data[0] / 20)
     opt.zero_grad()
     loss.backward()
     opt.step()
-
-
-
-
-
