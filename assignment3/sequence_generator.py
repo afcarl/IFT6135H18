11b5b05041da7065e0f72b356f329c277dc6163d
remilepriol <remi.lp.17@gmail.com>
2018-03-27 15:25:32 -0400

    Return an input and an output in the data loader

--- assignment3/sequence_generator.py
+++ assignment3/sequence_generator.py
@@ -7,15 +7,17 @@ import torch
 def generate_sequence(nb_batch, max_len, batch_size=1, cuda=False):
     # module = torch.cuda if cuda else torch
     for batch_idx in range(nb_batch):
+        seq_len = random.randint(1, max_len)
 
-        seq_len = random.randint(2, max_len + 1)
+        # Make the sequence longer by 1 to append the EOS
+        seq = (numpy.random.rand(seq_len+1, batch_size, 9) > 0.5).astype(numpy.float32)
 
-        seq = (numpy.random.rand(seq_len, batch_size, 9) > 0.5).astype(numpy.float32)
-
+        # Define the End Of Sequence character
         seq[-1] = 0
         seq[:, -1] = 0
         seq[-1, -1] = 1
 
-        seq = torch.autograd.Variable(torch.from_numpy(seq))
+        in_seq = torch.autograd.Variable(torch.from_numpy(seq))
+        out_seq = torch.autograd.Variable(torch.from_numpy(seq[:-1, :-1]))
 
-        yield seq
+        yield in_seq, out_seq
