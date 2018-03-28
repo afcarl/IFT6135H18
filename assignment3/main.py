427bbce92b8fce3415743b5e095e3f09831c8d72
remilepriol <remi.lp.17@gmail.com>
2018-03-28 16:26:48 -0400

    Change the logging location

--- assignment3/main.py
+++ assignment3/main.py
@@ -1,11 +1,11 @@
-import torch
-from torch.autograd import Variable
-from ntm import NTM
-from sequence_generator import *
 import datetime
 
-tb_plot = False
+import torch
+from ntm import NTM
+from sequence_generator import *
+from torch.autograd import Variable
 
+tb_plot = True
 
 if __name__ == "__main__":
     batch_size = 32
@@ -18,30 +18,27 @@ if __name__ == "__main__":
 
     if tb_plot:
         from tensorboardX import SummaryWriter
+
         now = datetime.datetime.now()
-        folder = f'/data/milatmp1/thomasva/DL_NTM/limited/logs/{now.day}_{now.month}/'
-        name = f'{now.hour}_{now.minute}_{now.second}'
-        name += f'_N={N}_M={M}_min_l={min_len}_batch={batch_size}_lr={lr}'
-        print(name)
+        folder = (f'logs/{now.month}_{now.day}/'
+                  f'{now.hour}_{now.minute}_{now.second}'
+                  f'_N={N}_M={M}_min_l={min_len}_batch={batch_size}_lr={lr}')
+        print(folder)
 
-        writer = SummaryWriter(log_dir=folder+name)
-
+        writer = SummaryWriter(log_dir=folder)
 
     seqgen = generate_inf_sequence(dim, min_len, max_len, batch_size=batch_size)
 
+    input_zero = Variable(torch.zeros(batch_size, dim + 1)).cuda()
 
-    input_zero = Variable(torch.zeros(batch_size, dim+1)).cuda()
-
-
-    ntm = NTM(N, M, dim+1, dim+1, batch_size=batch_size)
+    ntm = NTM(N, M, dim + 1, dim + 1, batch_size=batch_size)
     ntm.cuda()
 
     criterion = torch.nn.BCELoss()
     opt = torch.optim.Adam(ntm.parameters(), lr=1e-3)
 
-
     for step, input in enumerate(seqgen):
-        nb_samples = step*batch_size
+        nb_samples = step * batch_size
         loss = 0
         ntm.reset()
         acc = 0
@@ -49,20 +46,18 @@ if __name__ == "__main__":
             ntm.send(input[i, :, :])
 
         for i in range(input.size(0)):
-            #x = ntm.receive()
+            # x = ntm.receive()
             x = ntm.receive(input_zero)
             loss += criterion(x, input[i, :, :])
             acc += (x.round() == input[i, :, :]).float().mean()[0]
 
-
-
         if step % 25 == 0:
             print('Step:', step)
-            print('Loss:', loss.data[0]/input.size(0))
-            print('Accuracy:', acc.data[0]/input.size(0))
+            print('Loss:', loss.data[0] / input.size(0))
+            print('Accuracy:', acc.data[0] / input.size(0))
             if tb_plot:
-                writer.add_scalar('Loss', loss.data[0]/input.size(0), nb_samples)
-                writer.add_scalar('Accuracy', acc.data[0]/input.size(0), nb_samples)
+                writer.add_scalar('Loss', loss.data[0] / input.size(0), nb_samples)
+                writer.add_scalar('Accuracy', acc.data[0] / input.size(0), nb_samples)
 
         opt.zero_grad()
         loss.backward()
