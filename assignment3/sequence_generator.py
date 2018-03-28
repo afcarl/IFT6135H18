a522b4d85eba353f6083e599f92716830f76fe95
remilepriol <remi.lp.17@gmail.com>
2018-03-28 19:12:59 -0400

    Explain the generated input

--- assignment3/sequence_generator.py
+++ assignment3/sequence_generator.py
@@ -19,9 +19,10 @@ def generate_inf_sequence(min_len, max_len, dim=8, batch_size=1, cuda=True):
     batch_id = 0
     while True:
         batch_id += 1
+
         seq_len = random.randint(min_len, max_len)
 
-        # Make the sequence longer by 1 to append the EOS
+        # Make the sequence longer by 1 on the 1st and 3rd dimensions to append the EOS
         seq = (numpy.random.rand(seq_len + 1, batch_size, dim + 1) > 0.5).astype(numpy.float32)
 
         # Define the End Of Sequence character
