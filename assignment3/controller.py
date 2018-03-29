993fa1802dfa5bbc54dce24298abb297d590d7f3
remilepriol <remi.lp.17@gmail.com>
2018-03-28 20:17:43 -0400

    Implement LSTM controller

--- assignment3/controller.py
+++ assignment3/controller.py
@@ -1,17 +1,41 @@
+import torch
 from torch import nn
-from torch.nn import functional as F
 
 
-class Controller(nn.Module):
+class FeedForwardController(nn.Module):
 
-    def __init__(self, in_size, M, out_size, lstm=False):
-        super(Controller, self).__init__()
-        self.layer = nn.Linear(in_size, 100)
-        self.output_layer = nn.Linear(M, out_size)
+    def __init__(self, in_size, out_size, M):
+        super(FeedForwardController, self).__init__()
+        self.in_net = nn.Linear(in_size, 100)
+        self.out_net = nn.Linear(M, out_size)
 
     def forward(self, x):
-        h = self.layer(x)
-        return h
+        return self.in_net(x)
 
-    def compute_output(self, r):
-        return F.sigmoid(self.output_layer(r))
+    def compute_output(self, read):
+        return self.out_net(read)
+
+    def reset(self):
+        pass
+
+
+class LSTMController(nn.Module):
+
+    def __init__(self, in_size, out_size, M):
+        super(LSTMController, self).__init__()
+        self.lstm = nn.LSTMCell(in_size, 100)
+        self.hidden_state = nn.Parameter(torch.zeros(1, 100).cuda())
+        self.cell_state = nn.Parameter(torch.zeros(1, 100).cuda())
+        self.out_net = nn.Linear(M, out_size)
+
+    def forward(self, x):
+        self.hidden_state, self.cell_state = self.lstm(
+            x, (self.hidden_state, self.cell_state))
+        return self.hidden_state
+
+    def compute_out(self, read):
+        return self.out_net(read)
+
+    def reset(self):
+        self.hidden_state = nn.Parameter(torch.zeros(1, 100).cuda())
+        self.cell_state = nn.Parameter(torch.zeros(1, 100).cuda())
