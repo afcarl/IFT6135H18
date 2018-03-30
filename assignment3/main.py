cb61c47f8caab9009fc59c53f29560f15b090eb7
remilepriol <remi.lp.17@gmail.com>
2018-03-30 19:56:37 -0400

    Log accuracy vs sequence length

--- assignment3/main.py
+++ assignment3/main.py
@@ -63,54 +63,85 @@ if __name__ == "__main__":
     criterion = torch.nn.BCEWithLogitsLoss()
     opt = torch.optim.Adam(ntm.parameters(), lr=lr)
 
-    nb_samples = 0
-    for step, inp, out in seqgen:
+    try:
+        nb_samples = 0
+        for step, inp, out in seqgen:
 
-        nb_samples += batch_size
+            nb_samples += batch_size
 
-        ntm.reset()
+            ntm.reset()
 
-        loss = 0
-        acc = 0
-        for i in range(inp.size(0)):
-            ntm.send(inp[i])
-
-        for i in range(out.size(0)):
-            x = ntm.receive(input_zero)
-            loss += criterion(x, out[i])
-            acc += (F.sigmoid(x).round() == out[i]).float().mean()[0]
-
-        meanloss = loss.data[0] / out.size(0)
-        meanacc = acc.data[0] / out.size(0)
-
-        if step % 25 == 0:
-            print(f'Step: {step:<9}'
-                  f'Loss: {meanloss:<10.4f}'
-                  f'Accuracy: {meanacc:<10.4f}'
-                  f'Length: {out.size(0):<5}')
-            if tb_plot:
-                writer.add_scalar('Loss', meanloss, nb_samples)
-                writer.add_scalar('Accuracy', meanacc, nb_samples)
-
-        opt.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm(ntm.parameters(), 10)
-        opt.step()
-
-        if step % attention_period == 0:
-            attention = []
-            inp = create_sequence(seq_len=100, batch_size=1, cuda=cuda)
+            loss = 0
+            acc = 0
             for i in range(inp.size(0)):
                 ntm.send(inp[i])
-                attention.append(ntm.write_head.attention)
-            for i in range(inp.size(0) - 1):
+
+            for i in range(out.size(0)):
                 x = ntm.receive(input_zero)
-                attention.append(ntm.read_head.attention)
+                loss += criterion(x, out[i])
+                acc += (F.sigmoid(x).round() == out[i]).float().mean()[0]
 
-            attention = torch.stack(attention, dim=0)
-            # remove the batch axis and set the sequence on the x axis
-            attention = attention.squeeze().transpose(0, 1)
-            writer.add_image('Attention', attention, step)
+            meanloss = loss.data[0] / out.size(0)
+            meanacc = acc.data[0] / out.size(0)
 
-        if nb_samples > 2000000:
-            break
+            if step % 25 == 0:
+                print(f'Step: {step:<9}'
+                      f'Loss: {meanloss:<10.4f}'
+                      f'Accuracy: {meanacc:<10.4f}'
+                      f'Length: {out.size(0):<5}')
+                if tb_plot:
+                    writer.add_scalar('Loss', meanloss, nb_samples)
+                    writer.add_scalar('Accuracy', meanacc, nb_samples)
+
+            opt.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm(ntm.parameters(), 10)
+            opt.step()
+
+            if step % attention_period == 0:
+                attention = []
+                inp = create_sequence(seq_len=10, batch_size=1, cuda=cuda)
+                for i in range(inp.size(0)):
+                    ntm.send(inp[i])
+                    attention.append(ntm.write_head.attention)
+                for i in range(inp.size(0) - 1):
+                    x = ntm.receive(input_zero)
+                    attention.append(ntm.read_head.attention)
+
+                attention = torch.stack(attention, dim=0)
+                # remove the batch axis and set the sequence on the x axis
+                attention = attention.squeeze().transpose(0, 1)
+                writer.add_image('Attention', attention, step)
+
+            if nb_samples > 2000000:
+                break
+
+    except KeyboardInterrupt:
+        # record the accuracy on different sequence lengths
+        # we want to average over 20 sequences
+        # but the model has a predefined batch size
+        # that's a code problem but we'll use a for loop instead
+
+        nb_batches = 20 // batch_size
+        nb_test_samples = nb_batches * batch_size
+        print(f"Number of test batches: {nb_batches} "
+              f"and test samples : {nb_test_samples}")
+        for seq_len in range(10, 101, 10):
+            loss = 0
+            acc = 0
+            for batch in range(nb_batches):
+                inp = create_sequence(seq_len=seq_len, batch_size=batch_size, cuda=cuda)
+                for i in range(inp.size(0)):
+                    ntm.send(inp[i])
+
+                out = inp[:-1, :, :-1]
+                for i in range(out.size(0)):
+                    x = ntm.receive(input_zero)
+                    loss += criterion(x, out[i])
+                    acc += (F.sigmoid(x).round() == out[i]).float().mean()[0]
+
+            meanloss = loss.data[0] / out.size(0) / nb_batches
+            meanacc = acc.data[0] / out.size(0) / nb_batches
+            if tb_plot:
+                writer.add_scalar('Final Loss vs sequence length', meanloss, seq_len)
+                writer.add_scalar('Final Accuracy vs sequence length', meanacc, seq_len)
