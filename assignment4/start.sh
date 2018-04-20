ecf7242471989456e6111d9b9d3be1f8c65dec29
remilepriol <remi.lp.17@gmail.com>
2018-04-19 20:26:46 -0400

    Add sbatch file

---
+++ assignment4/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --time=24:00:00
+#SBATCH --job-name=celeba
+#SBATCH --output=celeba.out
+#SBATCH --qos=high
+#SBATCH --gres=gpu:1
+
+source activate pytorch36
+
+python my_dcgan_main.py --lanbda 0.5  --upsample nearest
+
+
