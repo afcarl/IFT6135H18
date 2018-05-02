02ebbffc2c266f006577291ea7d7b3f855a2cbf7
remilepriol <remi.lp.17@gmail.com>
2018-05-02 16:25:09 -0400

    Clean start.sh

--- assignment4/start.sh
+++ assignment4/start.sh
@@ -7,6 +7,4 @@
 
 source activate pytorch36
 
-past_logs=/data/milatmp1/lepriolr/gan/logs/dcgan/celebA/4_19/20_42_nsgan__lambda=0.5_citer=1_giter=1_beta1=0.5_upsample=nearest/
-
-python my_dcgan_main.py --lanbda 0.5  --upsample nearest --netG $past_logs/netG_epoch_15.pth --netD $past_logs/netD_epoch_15.pth
+python my_dcgan_main.py --lanbda 0.5  --upsample nearest
