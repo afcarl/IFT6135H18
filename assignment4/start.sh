bdc2797b93b8b6a3dafb2df267a46718ab7bab4b
remilepriol <remi.lp.17@gmail.com>
2018-05-04 11:57:39 -0400

    Change experiment

--- assignment4/start.sh
+++ assignment4/start.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #SBATCH --time=24:00:00
-#SBATCH --job-name=celeba
+#SBATCH --job-name=uniform1
 #SBATCH --output=celeba.out
-#SBATCH --qos=high
 #SBATCH --gres=gpu:1
+# add following line if on cedar
+##SBATCH --account=rpp-bengioy
 
 source activate pytorch36
 
-python my_dcgan_main.py --lanbda 1 --penalty fake
+
+python my_dcgan_main.py --lanbda 0 --penalty uniform
