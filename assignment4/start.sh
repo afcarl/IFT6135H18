ce6d2cdbe1625f890921ae74d9d36b67187e3dd3
remilepriol <remi.lp.17@gmail.com>
2018-05-09 17:24:05 -0400

    Update start script following doc How to Slurm

--- assignment4/start.sh
+++ assignment4/start.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 #SBATCH --time=24:00:00
-#SBATCH --job-name=uniform1
-#SBATCH --output=celeba.out
+#SBATCH --job-name=nogp
 #SBATCH --gres=gpu:1
 # add following line if on cedar
-##SBATCH --account=rpp-bengioy
+## SBATCH --account=rpp-bengioy
 
+export HOME=`getent passwd $USER | cut -d':' -f6`
+source ~/.bashrc
 source activate pytorch36
+export PYTHONUNBUFFERED=1
 
+echo Running on $HOSTNAME
 
-python my_dcgan_main.py --lanbda 0 --penalty uniform
+python my_dcgan_main.py --lanbda 1 --penalty real
